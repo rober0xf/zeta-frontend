a1653989a39b4da62a6b14033627f72248b60692
rober0xf <roberpili02@gmail.com>
2025-05-08 02:59:56 -0300

    home component

--- src/containers/pages/Home.tsx
+++ src/containers/pages/Home.tsx
@@ -1,5 +1,15 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import Layout from "../../hocs/layouts/Layout";
+
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <Layout>
+      <Navbar />
+      <main className="flex-1 ml-1">Home page</main>
+      <Footer />
+    </Layout>
+  );
 }
 
 export default Home;
