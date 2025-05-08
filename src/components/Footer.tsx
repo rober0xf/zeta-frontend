a647c5e422ea43b6ea3803b28a6135378d5d5680
rober0xf <roberpili02@gmail.com>
2025-05-08 03:00:20 -0300

    footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Gamma Labs
+        </p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
