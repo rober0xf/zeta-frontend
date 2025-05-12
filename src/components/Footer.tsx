2e807ef1cfd8ada6de74eac48e8daaacdcb2284c
rober0xf <roberpili02@gmail.com>
2025-05-12 00:49:14 -0300

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer sm:footer-horizontal footer-center bg-gray-50 text-base-content p-4">
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by Gamma Labs
+          Copyright © {new Date().getFullYear()} - All right reserved by Gamma
+          Labs
         </p>
       </aside>
     </footer>
