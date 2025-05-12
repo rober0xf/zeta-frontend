112aea9246d7138ece40c33407b15ad61ad12d52
rober0xf <roberpili02@gmail.com>
2025-05-12 00:49:23 -0300

    some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import Error404 from "./containers/errors/Error404";
 import Home from "./containers/pages/Home";
+import About from "./containers/pages/About";
+import Models from "./containers/pages/Models";
+import Pricing from "./containers/pages/Pricing";
+import Research from "./containers/pages/Research";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -12,6 +16,10 @@ function App() {
           {/* we define the routes here */}
           <Route path="*" element={<Error404 />} />
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/models" element={<Models />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/research" element={<Research />} />
         </Routes>
       </Router>
     </Provider>
